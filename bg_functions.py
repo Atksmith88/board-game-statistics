c07a1c5f07ccc601c8b693155db5f8ef0aec3f62
Andrew <pi_r_round@hotmail.com>
2020-02-14 06:11:22 -0600

    Created and Updated README.ipynb, Added images folder and files, added multiple models and tuning.

--- bg_functions.py
+++ bg_functions.py
@@ -1,3 +1,5 @@
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.model_selection import GridSearchCV
 
 
 def category_combine(df, columns=None, final_column=None):
@@ -15,3 +17,56 @@ def category_combine(df, columns=None, final_column=None):
         columns.remove(final_column)
     df.drop(columns=columns, inplace=True)
     
+
+def create_model(estimator, X_train, y_train,
+                 X_test, y_test, score=['MSE', 'R2']):
+    """Create a model from a given estimator.
+    
+    INPUTS:
+    estimator: Instantiated estimator for the model.
+    X_train, X_test, y_train, y_test: Train/Test data to be used.
+    score: List or single item for score output. (Default 'MSE' and 'R2').
+    
+    OUTPUTS:
+    Selected score(s) printed on screen.
+    """
+    estimator.fit(X_train, y_train)
+    y_pred = estimator.predict(X_test)
+    print(estimator)
+    for score in score:
+        if score == 'MSE':
+            print('MSE:', mean_squared_error(y_test, y_pred))
+        elif score == 'R2':
+            print('R2:', r2_score(y_test, y_pred))
+        else:
+            print('No valid score selected.') 
+
+
+def gridsearch_model(estimator, X_train, y_train, X_test, y_test, param_grid,
+                     scoring='r2', cv=3, title=None, verbose=0):
+    """This function will fit a model using the provided classifier, dataset,
+    and parameter grid. It uses GridSearchCV to determine the best model.
+
+    INPUTS:
+    estimator: Estimator to be fit.
+    X_train, X_test, y_train, y_test: Train/Test data to be used.
+    param_grid: Parameter grid to be used for the gridsearch.
+    scoring: Scoring method to be used for the gridsearch.
+    cv: Number of cross-validations.
+    title: Title to print for clarity when reviewing outputs.
+
+    OUTPUTS:
+    Returns the best estimator model from GridSearch.
+    Prints title and score to screen.
+    """
+    gridsearch = GridSearchCV(estimator=estimator, param_grid=param_grid,
+                              cv=cv, scoring=scoring, verbose=verbose)
+    gridsearch.fit(X_train, y_train)
+    if title:
+        print(title)
+    print("BEST PARAMS:")
+    print(gridsearch.best_params_)
+    print("\nBest", scoring.capitalize(), "Score:",
+          round(gridsearch.best_score_, 4))
+
+    return gridsearch.best_estimator_
